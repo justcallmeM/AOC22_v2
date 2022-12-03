ba3d71d870d2e38a3eef3b3daba71cffb8789c2d
Mindaugas Pikelis <36482664+justcallmeM@users.noreply.github.com>
2022-12-03 15:26:47 +0200

    day3 prep

---
+++ Days/3/Day3.cs
@@ -0,0 +1,15 @@
+﻿namespace AOC22.Days._3
+{
+    using Constants;
+    using Library;
+
+    public static class Day3
+    {
+        public static void Execute()
+        {
+            List<string> lines = ReadMethods.ReadFileIntoList(InputPath.DAY3_INPUT_PATH);
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AOC22
+
+Advent of code 2022 with C#
+The code here during the days is not final and will be refactored.
