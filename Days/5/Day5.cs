b902875351ab7fb507bffd1cc7985598c2276fd1
Mindaugas Pikelis <36482664+justcallmeM@users.noreply.github.com>
2022-12-07 23:58:19 +0200

    day5 scramble, yikes

--- Days/5/Day5.cs
+++ Days/5/Day5.cs
@@ -116,7 +116,74 @@
                 stacks.Add(stackToAdd);
             }
 
+            List<string> commands = new();
 
+            int indexOfEmptyLine = list.IndexOf(string.Empty);
+
+            commands = list.Skip(indexOfEmptyLine + 1).ToList();
+
+
+            foreach(var command in commands)
+            {
+                (int, int, int) movementInfo = DigestCommandInto_Amount_From_To(command);
+
+                //MoveCreates9000(stacks, movementInfo.Item1, movementInfo.Item2, movementInfo.Item3); //part1
+                MoveCrates9001(stacks, movementInfo.Item1, movementInfo.Item2, movementInfo.Item3); //part2
+            }
+
+            string answer = GetTopCratesFromEachStack(stacks);
+
+            Console.WriteLine(answer);
+        }
+
+        public static (int, int, int) DigestCommandInto_Amount_From_To(string command)
+        {
+            string[] commandParts = command.Split(' ');
+
+            int amount = int.Parse(commandParts[1]);
+            int from = int.Parse(commandParts[3]) - 1;
+            int to = int.Parse(commandParts[5]) - 1;
+
+            return (amount, from, to);
+        }
+
+        public static void MoveCreates9000(List<Stack<char>> stacks, int amount, int from, int to)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                char crate = stacks[from].Pop();
+
+                stacks[to].Push(crate);
+            }
+        }
+
+        public static void MoveCrates9001(List<Stack<char>> stacks, int amount, int from, int to)
+        {
+            List<char> tempCrateStorage = new();
+
+            for (int i = 0; i < amount; i++)
+            {
+                tempCrateStorage.Add(stacks[from].Pop());
+            }
+
+            tempCrateStorage.Reverse();
+
+            for (int i = 0; i < amount; i++)
+            {
+                stacks[to].Push(tempCrateStorage[i]);
+            }
+        }
+
+        public static string GetTopCratesFromEachStack(List<Stack<char>> stacks)
+        {
+            string topCrates = string.Empty;
+
+            foreach(Stack<char> stack in stacks)
+            {
+                topCrates += stack.Pop();
+            }
+
+            return topCrates;
         }
 
     }
